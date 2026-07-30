3ff79117bfc063b885099d63161ca37597e6271d
sajjad <sajjadonjob@gmail.com>
2026-07-30 23:12:41 +0500

    Debug message updated

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    qDebug() << "Initialization Startd";
+    qDebug() << "System initialization started";
     QQmlApplicationEngine engine;
     QObject::connect(
         &engine,
