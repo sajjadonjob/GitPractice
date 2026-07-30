04ce75f4bd6e12f76eb43b02f665f34585abc887
sajjad <sajjadonjob@gmail.com>
2026-07-30 23:22:13 +0500

    Typo fixed

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    qDebug() << "System initialization started";
+    qDebug() << "System Initialization Started.";
     QQmlApplicationEngine engine;
     QObject::connect(
         &engine,
